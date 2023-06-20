aa94673ba59d56b91c15e6e6fd17f7132e74193b
yura-galoyan <galoyanyur@gmail.com>
2023-06-20 21:19:55 +0400

    asd

--- sfmlGame/main.cpp
+++ sfmlGame/main.cpp
@@ -1,12 +1,23 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "controller.hpp"
 void test(sf::RenderWindow& window);
 
 void handleEvents(sf::Event& event, sf::RenderWindow& window);
 
 int main(){
+    std::string fontPath = "../Font/";
     sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
     sf::CircleShape shape(100.f);
+    sf::Font font; 
+    if (!font.loadFromFile(fontPath + "Mythical_Prince.ttf")) {
+        if (!font.loadFromFile("../" + fontPath + "Mythical_Prince.ttf")) {
+            std::cout << "could not load mythical prince font file" << std::endl;
+        }
+    }
+    sf::Text text("hello",font);
+    text.setOutlineThickness(2);
+    text.setOutlineColor(sf::Color::Red);
     shape.setFillColor(sf::Color::Green);
 
 
@@ -19,8 +30,7 @@ int main(){
         window.clear();
 
         test(window);
-
-        window.draw(shape);
+        window.draw(text);
         window.display();
     }
 
