07dbbf849cb0b4bb514541aeb63e596731592d78
yura-galoyan <galoyanyur@gmail.com>
2023-06-22 10:12:42 +0400

    asdsda

--- sfmlGame/main.cpp
+++ sfmlGame/main.cpp
@@ -5,13 +5,22 @@
 #include "controller.hpp"
 #include "stats.hpp"
 
-sf::Vector2<float> start{};
-sf::Vector2<float> end{};
+using Dot = sf::RectangleShape;
+using DotVector = std::vector<Dot>;
+
 using Line  = std::array<sf::Vertex,2>;
 using LinesVec = std::vector<Line>;
 
+Dot start{};
+Dot end{};
 
-void calculateDistance(Controller::KeyboardEvents keyEvents, bool&, sf::RenderWindow&, LinesVec&);
+bool operator!=(Dot l, Dot r){
+    bool ret = l.getPosition().x == r.getPosition().x &&
+    l.getPosition().y == r.getPosition().y;
+    return !ret;
+}
+
+void calculateDistance(Controller::KeyboardEvents keyEvents, bool&, sf::RenderWindow&, DotVector&);
 int main(){
     sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
     window.setKeyRepeatEnabled(false);
@@ -23,47 +32,39 @@ int main(){
     controller.initKeys();
     Stats stats(window,"Mythical_Prince.ttf");
 
-    LinesVec lines;
-
+    DotVector dotes;
+    start.setSize(sf::Vector2<float>{15.0f, 15.0f});
+    start.setFillColor(sf::Color::Red);
     while (window.isOpen()){
         controller.handleEvents(event,window);
-        calculateDistance(controller.getKeyEvents(),enabl,window, lines);
+        calculateDistance(controller.getKeyEvents(),enabl,window, dotes);
 
         window.clear();
-        
-        if(enabl){
-            keepDrawing = true;
-        }
-
-        if(enabl || keepDrawing){
-            for(auto line :lines){
-                window.draw(line.data(),2,sf::Lines);
-            }
-            enabl = false;
+        for(auto dot : dotes){
+            window.draw(dot);
         }
         stats.showAllStats(controller.getKeyEvents());
         window.display();
-
     }
 
     return 0;
 }
 
-void calculateDistance(Controller::KeyboardEvents keyEvents, bool& enabled, sf::RenderWindow& window, LinesVec& vec){
-    static Line line;
+void calculateDistance(Controller::KeyboardEvents keyEvents, bool& enabled, sf::RenderWindow& window, DotVector& vec){
 
-    if(keyEvents.mouseLeft.pressed){
-        start.x = sf::Mouse::getPosition(window).x;
-        start.y = sf::Mouse::getPosition(window).y;
-        line[0] = start;
+
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+        start.setPosition(sf::Mouse::getPosition(window).x,sf::Mouse::getPosition(window).y);
+        if(!vec.empty()){
+            if(vec.back() != start){
+                vec.push_back(start);
+            }
+        }
+        else{
+            vec.push_back(start);
+        }
     }
-    if(keyEvents.mouseLeft.released){
-        end.x = sf::Mouse::getPosition(window).x;
-        end.y = sf::Mouse::getPosition(window).y;
-        line[1] = end;
-        enabled = true;
-        vec.push_back(line);
-    }
+
     if(keyEvents.mouseRight.pressed){
         if(!vec.empty()){
             vec.pop_back();
