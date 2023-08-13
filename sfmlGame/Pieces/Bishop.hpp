0d4847f1a1f984c18d0e2d4afa6e20f58063dfeb
yura-galoyan <galoyanyur@gmail.com>
2023-08-13 17:38:19 +0400

    configured loading screen class, added sprite class, configured Texture Manager class

--- sfmlGame/Pieces/Bishop.hpp
+++ sfmlGame/Pieces/Bishop.hpp
@@ -8,9 +8,9 @@ class Bishop : public Piece
 public:
     Bishop() = default;
     Bishop(const Bishop& k){}
-    virtual std::unique_ptr<Piece> clone() const override ;
-    virtual void move(Coords xy) override ;
-    virtual void draw(Window& window) override ;
+    std::unique_ptr<Piece> clone() const override ;
+    void move(Coords xy) override ;
+    void draw(Window& window, Texture& texture) override ;
 }; 
 
 #endif //BISHOP_HPP
